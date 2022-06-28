b243540f734555f08d68f372cf7e8f9d3e5808df
CHAITANYA735 <97425744+CHAITANYA735@users.noreply.github.com>
2022-06-28 22:49:02 +0530

    n=int(input()) for i in range(n-1,1,-1):     r=0     temp=i     while i>0:         d=i%10         r=r*10+d         i=i//10     if temp==r:         y=r         break dif1=abs(n-y) k=n+1 while True:     r1=0     temp=k     while temp>0:         d1=temp%10         r1=r1*10+d1         temp=temp//10     if k==r1:         z=r1         break     k=k+1 dif2=abs(n-z) if dif2==dif1:     print(y,z,end=' ') elif dif2>dif1:     print(y) else:     print(z)

--- Closest_Palindrome_.py
+++ Closest_Palindrome_.py
@@ -1,27 +1,31 @@
-def is_pali(o):
-    t=o
-    rev=0
-    while o!=0:
-        r=o%10
-        rev=rev*10+r
-        o//=10
-    if rev==t:
-        return 1
-    else:
-        return 0
 n=int(input())
-for i in range(1,10000):
-    if is_pali(n+i):
-        m=n+i
+for i in range(n-1,1,-1):
+    r=0
+    temp=i
+    while i>0:
+        d=i%10
+        r=r*10+d
+        i=i//10
+    if temp==r:
+        y=r
         break
-for i in range(1,10000):
-    if is_pali(n-i):
-        l=n-i
+dif1=abs(n-y)
+k=n+1
+while True:
+    r1=0
+    temp=k
+    while temp>0:
+        d1=temp%10
+        r1=r1*10+d1
+        temp=temp//10
+    if k==r1:
+        z=r1
         break
-if n-l==m-n:
-    print(l,m)
-elif n-l>m-n:
-    print(m)
+    k=k+1
+dif2=abs(n-z)
+if dif2==dif1:
+    print(y,z,end=' ')
+elif dif2>dif1:
+    print(y)
 else:
-    print(l)
-    
+    print(z)
