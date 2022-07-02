32ae5cf6985b1dd3dbc62a9910130c90208dcd6c
CHAITANYA735 <97425744+CHAITANYA735@users.noreply.github.com>
2022-07-02 10:43:26 +0530

    n=int(input()) m=n*n a=[] b=[] while n!=0:     r=n%10     a.append(r)     n//=10 while m!=0:     r1=m%10     b.append(r1)     m//=10 c=0 for i in range(len(a)):     if a[i] in b:         c+=1 if c==len(a):     print('Automorphic Number') else:     print('Not an Automorphic Number')

--- Automorphic_Number.py
+++ Automorphic_Number.py
@@ -1,19 +1,20 @@
 n=int(input())
 m=n*n
+a=[]
+b=[]
+while n!=0:
+    r=n%10
+    a.append(r)
+    n//=10
+while m!=0:
+    r1=m%10
+    b.append(r1)
+    m//=10
 c=0
-org=n
-while n>0:
-    d=n%10
-    c=c+1
-    n=n//10
-r=0
-temp=m
-while m>0:
-    rem=m%10
-    r=r*10+rem
-    m=m//10
-p=temp%(10**c)
-if p==org:
+for i in range(len(a)):
+    if a[i] in b:
+        c+=1
+if c==len(a):
     print('Automorphic Number')
 else:
     print('Not an Automorphic Number')
